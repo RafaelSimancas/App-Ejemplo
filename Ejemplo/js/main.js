543852bcf2203640a048cabc85c32f0d7fae652c
RafaelSimancas <38543500+RafaelSimancas@users.noreply.github.com>
2018-05-24 16:35:19 -0600

    Update main.js

--- Ejemplo/js/main.js
+++ Ejemplo/js/main.js
@@ -144,7 +144,8 @@ function eliminarForm(tx) {
 	alert("haciendo el trabajo id =" +$.id);
 	/*var sql = "DELETE * FROM agenda_curso WHERE id=" +$.id;
 	tx.executeSql(sql);*/
-     tx.executeSql('DELETE FROM agenda_curso WHERE id='+$.id);
+     //tx.executeSql('DELETE FROM agenda_curso WHERE id='+$.id);
+	tx.executeSql('DROP TABLE IF EXISTS agenda_curso');
 		$.mobile.changePage("#home");
 }
 
