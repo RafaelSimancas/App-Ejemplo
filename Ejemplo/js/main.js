0a57046b32f036362c9063e3216222025cd5c7e5
RafaelSimancas <38543500+RafaelSimancas@users.noreply.github.com>
2018-05-24 16:14:43 -0600

    Update main.js

--- Ejemplo/js/main.js
+++ Ejemplo/js/main.js
@@ -141,8 +141,10 @@ function cargaDatosSuccess(tx, results){
 
 /*Eliminando*/
 function eliminarForm(tx) {
-	alert("haciendo el trabajo x3");
-    tx.executeSql('DELETE * FROM agenda_curso WHERE id='+$.id, [], errorDB);
+	alert("haciendo el trabajo id =" +$.id);
+	var sql = "DELETE * FROM agenda_curso WHERE id='" +$.id+ "'";
+	tx.executeSql(sql);
+    //tx.executeSql('DELETE * FROM agenda_curso WHERE id='+$.id, [], errorDB);
 }
 
 
